27a0338a2ecf422924de96bfb3d00306d38edbe7
hendisantika <hendisantika@yahoo.co.id>
2019-07-19 04:40:05 +0700

    #6 - Add PersonServiceImpl

---
+++ src/main/java/com/hendisantika/springbootehcache/service/PersonServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootehcache.service;
+
+import com.hendisantika.springbootehcache.model.Person;
+import com.hendisantika.springbootehcache.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-ehcache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-19
+ * Time: 04:39
+ */
+@Service
+public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public List<Person> getAll() {
+        return personRepository.findAll();
+    }
+
+}
